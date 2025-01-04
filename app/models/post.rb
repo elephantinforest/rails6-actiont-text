2ce8cbc3aa58fbc215beea0e0d19bc51cb57721c
elephantinforest <sikurowaiyado@gmail.com>
2025-01-04 17:23:35 +0900

    add validation count

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,16 +3,31 @@ class Post < ApplicationRecord
 
     validates :title, length: { maximum: 32  }, presence: true
 
+    validate :validate_content_max_register
     validate :validate_content_length
     validate :validate_content_attachment_byte_size
 
 
-    MAX_CONTENT_LENGTH = 50
+    MAX_CONTENT_LENGTH = 80
     ONE_KILOBYTE = 1024
     MEGA_BYTES = 3
     MAX_CONTENT_ATTACHEMENT_BYTE_SIZE = MEGA_BYTES * 1_000 * ONE_KILOBYTE
+    MAX_CONTENT_REGISTER = 4
 
     private
+
+    def validate_content_max_register
+    count = content.body.attachables.grep(ActiveStorage::Blob).size
+    if count > MAX_CONTENT_REGISTER
+      errors.add(
+        :base,
+        :content_registers_size_is_too_big,
+        registers: count,
+        max_registers: MAX_CONTENT_REGISTER
+      )
+    end
+    end
+
 
     def validate_content_attachment_byte_size
         content.body.attachables.grep(ActiveStorage::Blob).each do |attachable|
